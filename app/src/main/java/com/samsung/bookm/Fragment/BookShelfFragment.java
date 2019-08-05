0fa1a349cd709f2e2e19278a3b12a186b398ac29
Ta Bao Thang 20153544 <thang.tb153544@sis.hust.edu.vn>
2019-08-05 21:25:17 +0700

    +add reminder

--- app/src/main/java/com/samsung/bookm/Fragment/BookShelfFragment.java
+++ app/src/main/java/com/samsung/bookm/Fragment/BookShelfFragment.java
@@ -1,30 +1,46 @@
 package com.samsung.bookm.Fragment;
 
 
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.samsung.bookm.Activity.SearchActivity;
 import com.samsung.bookm.R;
+import com.samsung.bookm.Util.PdfReaderUtils;
+
+import static android.app.Activity.RESULT_OK;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class BookShelfFragment extends Fragment {
     Toolbar toolbar;
+    PdfReaderUtils pdfReaderUtils = new PdfReaderUtils(getContext());
+
+    private final static int REQUEST_CODE = 42;
+    public static final int PERMISSION_CODE = 42042;
+    public static final String SAMPLE_FILE = "sample.pdf";
+    public static final String READ_EXTERNAL_STORAGE = "android.permission.READ_EXTERNAL_STORAGE";
 
     public BookShelfFragment() {
         // Required empty public constructor
@@ -34,12 +50,10 @@ public class BookShelfFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        Log.d("SVMC", "onCreateView BOOK");
         // Inflate the layout for this fragment
         View v=  inflater.inflate(R.layout.fragment_book_shelf, container, false);
-        setHasOptionsMenu(true);
-        toolbar = v.findViewById(R.id.toolbar);
-        ((AppCompatActivity)getActivity()).setSupportActionBar(toolbar);
-        ((AppCompatActivity)getActivity()).getSupportActionBar().setTitle(R.string.title_book_shelf_activity);
+
 
         return v;
     }
@@ -47,7 +61,10 @@ public class BookShelfFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-
+        Log.d("SVMC", "onViewCreated BOOK");
+        toolbar = view.findViewById(R.id.toolbar);
+        ((AppCompatActivity)getActivity()).setSupportActionBar(toolbar);
+        toolbar.setTitle(R.string.title_book_shelf_activity);
 
 
         toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
@@ -55,6 +72,7 @@ public class BookShelfFragment extends Fragment {
             public boolean onMenuItemClick(MenuItem item) {
                 switch (item.getItemId()){
                     case R.id.action_add:
+                        pickFile();
                         return true;
                     case  R.id.action_more:
                         return true;
@@ -63,7 +81,7 @@ public class BookShelfFragment extends Fragment {
                         startActivity(intent);
                         return true;
                     default:
-                       return BookShelfFragment.super.onOptionsItemSelected(item);
+                        return BookShelfFragment.super.onOptionsItemSelected(item);
                 }
             }
         });
@@ -72,7 +90,54 @@ public class BookShelfFragment extends Fragment {
 
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
-        super.onCreateOptionsMenu(menu, inflater);
-        toolbar.inflateMenu(R.menu.book_shelf_menu);
+        inflater.inflate(R.menu.book_shelf_menu, menu);
+        super.onCreateOptionsMenu(menu,inflater);
+    }
+
+
+    // xin quyen doc file
+    void pickFile() {
+        int permissionCheck = ContextCompat.checkSelfPermission(getContext(),
+                READ_EXTERNAL_STORAGE);
+
+        if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(
+                    getActivity(),
+                    new String[]{READ_EXTERNAL_STORAGE},
+                    PERMISSION_CODE
+            );
+
+            return;
+        }
+
+        launchPicker();
+    }
+
+    // tim kiem file
+    void launchPicker() {
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("application/pdf");
+        try {
+            startActivityForResult(intent, REQUEST_CODE);
+        } catch (ActivityNotFoundException e) {
+            //alert user that file manager not working
+            Toast.makeText(getContext(), "Can not open file!!!", Toast.LENGTH_SHORT).show();
+        }
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CODE){
+            if(resultCode == RESULT_OK){
+                Uri uri = data.getData();
+                pdfReaderUtils.displayFromUri(uri);
+            }
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d("SVMC", "DESTROY BOOK");
     }
 }
--- app/src/main/java/com/samsung/bookm/Fragment/ScheduleFragment.java
+++ app/src/main/java/com/samsung/bookm/Fragment/ScheduleFragment.java
@@ -1,20 +1,31 @@
 package com.samsung.bookm.Fragment;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.samsung.bookm.Activity.AddReminderActivity;
+import com.samsung.bookm.Activity.SearchActivity;
 import com.samsung.bookm.R;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class ScheduleFragment extends Fragment {
+    Toolbar toolbar;
 
 
     public ScheduleFragment() {
@@ -26,7 +37,40 @@ public class ScheduleFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_schedule, container, false);
+        View v = inflater.inflate(R.layout.fragment_schedule, container, false);
+
+
+        return v;
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        toolbar = view.findViewById(R.id.toolbar);
+        ((AppCompatActivity)getActivity()).setSupportActionBar(toolbar);
+        setHasOptionsMenu(true);
+        toolbar.setTitle(R.string.title_schedule_activity);
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.action_add:
+                        Intent intent = new Intent(getActivity(), AddReminderActivity.class);
+                        startActivity(intent);
+                        return true;
+                    case  R.id.action_more:
+                        return true;
+                    default:
+                        return ScheduleFragment.super.onOptionsItemSelected(item);
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.schedule_menu, menu);
+
+    }
 }
