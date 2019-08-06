17f153f16e4da00a65dccf8be3a418d8eb08bcdf
Ta Bao Thang 20153544 <thang.tb153544@sis.hust.edu.vn>
2019-08-06 22:14:58 +0700

    add night/mode setting

--- app/src/main/java/com/samsung/bookm/Fragment/SettingFragment.java
+++ app/src/main/java/com/samsung/bookm/Fragment/SettingFragment.java
@@ -1,23 +1,33 @@
 package com.samsung.bookm.Fragment;
 
 
+import android.content.Context;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.QuickContactBadge;
 
+import com.samsung.bookm.Interface.ISettingCallBack;
 import com.samsung.bookm.R;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class SettingFragment extends Fragment {
-
-
+    ISettingCallBack iSettingCallBack;
     public SettingFragment() {
+        // Required empty public constructor
+    }
+    public SettingFragment(ISettingCallBack context) {
+        iSettingCallBack = context;
         // Required empty public constructor
     }
 
@@ -26,7 +36,24 @@ public class SettingFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_setting, container, false);
+        View v= inflater.inflate(R.layout.fragment_setting, container, false);
+        Toolbar toolbar = v.findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.title_setting_activity);
+        return v;
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        Button sw_night_mode = view.findViewById(R.id.sw_night_mode);
+        sw_night_mode.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(iSettingCallBack==null){
+                    iSettingCallBack = (ISettingCallBack) getContext();
+                }
+                iSettingCallBack.alternateNightMode(iSettingCallBack.getCurrentNightMode());
+            }
+        });
+    }
 }
